7dc22a2f747996092a1973ff9356b13e9a132b6e
Santosh Kumar Sethy <154328801+santoshksethy@users.noreply.github.com>
2025-08-25 19:47:10 +0530

    tried @RequestParam

---
+++ SpringRestProj07-RequestParams/src/main/java/com/app/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer-api")
+public class BaseController
+{
+	@GetMapping("/get")
+	public ResponseEntity<String> get(@RequestParam("cno") Integer no,@RequestParam String name)
+	{
+		return new ResponseEntity<String>("Customer id: "+no+"\nCustomer name: "+name,HttpStatus.OK);
+	}
+	
+
+}
